f6fc04c911b5d57e398e178916fc7475eaf3cc6d
Miles Gould <miles@assyrian.org.uk>
2013-07-19 14:49:49 +0100

    Print out best classifier score for each metric.

--- analysis/xval.py
+++ analysis/xval.py
@@ -10,26 +10,35 @@ from analysis import read_files, classifiers
 def accuracy(actual, predictions, weights):
     return float(sum((actual == predictions) * weights)/sum(weights))
 
+def xval_score(cfr, target, train, weights):
+    """Cross-validate a single classifier"""
+    #Simple K-Fold cross validation. 5 folds.
+    cv = cross_validation.KFold(len(train), n_folds=5, indices=False)
+    #iterate through the training and test cross validation segments and
+    #run the classifier on each one, aggregating the results into a list
+    results = []
+    for traincv, testcv in cv:
+        trained = cfr.fit(train[traincv], target[traincv])
+        predictions = trained.predict(train[testcv])
+        score = accuracy(target[testcv], predictions, weights[testcv])
+        results.append(score)
+    return np.array(results).mean()
+
 def xval(target, train, weights):
+    """Cross-validate all classifiers, and print out their scores"""
     cs = classifiers()
     max_name_length = max([len(name) for name in cs.keys()])
     print "Percentage accuracy: higher numbers are better"
+    best_score = 0
+    best_name = "Always wrong"
     for name in cs.keys():
-        cfr = cs[name]
-        #Simple K-Fold cross validation. 5 folds.
-        cv = cross_validation.KFold(len(train), n_folds=5, indices=False)
-        #iterate through the training and test cross validation segments and
-        #run the classifier on each one, aggregating the results into a list
-        results = []
-        for traincv, testcv in cv:
-            trained = cfr.fit(train[traincv], target[traincv])
-            predictions = trained.predict(train[testcv])
-            # print predictions
-            score = accuracy(target[testcv], predictions, weights[testcv])
-            results.append(score)
+        score = xval_score(cs[name], target, train, weights)
         #print out the mean of the cross-validated results
-        print "{}  {}".format(name.ljust(max_name_length),
-                np.array(results).mean())
+        print "{}  {}".format(name.ljust(max_name_length), score)
+        if score > best_score:
+            best_score = score
+            best_name = name
+    print "Best: {} {}".format(best_name, best_score)
 
 def excess_mispredictions(dataset):
     excess_misprediction_frequency = abs(2 * dataset[:, prob] - 1)
